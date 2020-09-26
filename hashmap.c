2c50c7ef8f7e82807a74cc054c72cbc00ddae80a
repl.it user <replituser@example.com>
2020-09-26 23:37:30 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -49,11 +49,11 @@ void insertMap(HashMap * map, char * key, void * value) {
   Pair* x = createPair(key,value);
 
   long i;
-  for (i=h;i<map->capacity;i++){
+  for (i=0;i<map->capacity;i++){
     if (i>=map->capacity-1){
       i=0;
     }
-    if ((map->buckets[h] == NULL) && (x->key != NULL)){
+    if ((map->buckets[h] == NULL) || (map->buckets[h]->key == NULL)){
         map->buckets[h]=x;
         break;
     }
