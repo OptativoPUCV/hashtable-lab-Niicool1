65ee09b21b80eba0b1a8fc2d165abb068b3fd096
repl.it user <replituser@example.com>
2020-09-26 21:54:30 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -45,8 +45,9 @@ int is_equal(void* key1, void* key2){
 
 
 void insertMap(HashMap * map, char * key, void * value) {
-  long hash_= hash(key,map->capacity);
-  long i;
+  //long hash_= hash(key,map->capacity);
+  printf("hola mundo");
+  /*long i;
   for (i=0;i<map->capacity;i++){
     if ((map->buckets[i] == NULL) && (map->buckets[i]->key == NULL)){
         map->buckets[hash_]->key=key;
@@ -56,7 +57,7 @@ void insertMap(HashMap * map, char * key, void * value) {
         continue;
     }
     map->size=map->size +1;
-  }
+  }*/
 
 }
 
